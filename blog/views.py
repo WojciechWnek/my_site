3860811f43bdd4b35486404f9743350654c7292f
Wojciech Wnek <wojciech.wnek97@gmail.com>
2023-07-06 14:58:51 +0200

    Add dynamic posts pages

--- blog/views.py
+++ blog/views.py
@@ -71,13 +71,25 @@ all_posts = [
     }
 ]
 
+def get_date(post):
+    return post.get('date')
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts,key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
-    return render(request, "blog/all_posts.html")
+    return render(request, "blog/all_posts.html",{
+        "all_posts": all_posts
+    })
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    iddentified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": iddentified_post
+    })
