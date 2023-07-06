8462d2f457964c7ec1e1b053a9c45a69654631ea
Wojciech Wnek <wojciech.wnek97@gmail.com>
2023-07-06 14:04:38 +0200

    Add html, css, images

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def starting_page(request):
+    return render(request, "blog/index.html")
+
+
+def posts(request):
+    return render(request, "blog/all_posts.html")
+
+
+def post_detail(request, slug):
+    text = "Hello " + slug
+
+    return HttpResponse(text)
